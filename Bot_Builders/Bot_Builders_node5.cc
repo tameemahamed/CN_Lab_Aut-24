1ac7e1c3df40e8e00b524ec4660cbeceb6c7b256
tameemahamed <98138554+tameemahamed@users.noreply.github.com>
2024-10-28 23:48:07 +0600

    updated lab 5

--- Bot_Builders/Bot_Builders_node5.cc
+++ Bot_Builders/Bot_Builders_node5.cc
@@ -2,22 +2,16 @@
 #include <omnetpp.h>
 #include <stdio.h>
 
-
 using namespace omnetpp;
-
-#include "frame_BB_m.h"
-
 class Bot_Builders_node5 : public cSimpleModule
 {
   private:
-    int energy;
-    int counter;
+    int limit;
     int src,dst;
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    virtual void forwardMessage(frame_BB *msg);
-    virtual frame_BB *generateMessage();
+    virtual void forwardMessage(cMessage *msg);
 };
 
 // The module class needs to be registered with OMNeT++
@@ -25,63 +19,76 @@ Define_Module(Bot_Builders_node5);
 
 void Bot_Builders_node5::initialize()
 {
-    counter = par("limit");
-    int src = par("source");
+    limit = par("limit");
+    src= par("source");
+    dst=par("destination");
     if(getIndex() == src) {
-        frame_BB *msg = generateMessage();
+        char msgname[20];
+        sprintf(msgname,"Lab5Msg");
+        cMessage *msg = new cMessage(msgname);
         scheduleAt(0.0, msg);
 
-    }
+    }/*
+    if (par("sendMsgOnInit").boolValue() == true) {
+            EV << "Sending initial message\n";
+            cMessage *msg = new cMessage("tictocMsg");
+            send(msg, "out");
+        }*/
+    // Set a timer to delete this message after 3 seconds
+
 }
 
 void Bot_Builders_node5::handleMessage(cMessage *msg)
 {
-    frame_BB *ttmsg = check_and_cast<frame_BB *>(msg);
-
-        if (ttmsg->getDestination() == getIndex()) {
-            // Message arrived.
-            EV << "Message " << ttmsg << " arrived after " << ttmsg->getHopCount() << " hops.\n";
-            bubble("ARRIVED, starting new one!");
-            delete ttmsg;
-
-            // Generate another one.
-            EV << "Generating another message: ";
-            frame_BB *newmsg = generateMessage();
-            EV << newmsg << endl;
-            forwardMessage(newmsg);
-        }
-        else {
-            // We need to forward the message.
-            forwardMessage(ttmsg);
-        }
+    // Check if the message is the delete timer
+    if(getIndex()==dst){
+        EV << "Message " << msg << " arrived.\n";
+        delete msg;
+    }
+    else{
+        forwardMessage(msg);
+    }
 
 }
 
-void Bot_Builders_node5::forwardMessage(frame_BB *msg)
-{
+void Bot_Builders_node5::forwardMessage(cMessage *msg)
+{/*
     // In this example, we just pick a random gate to send it on.
     // We draw a random number between 0 and the size of gate `gate[]'.
-    msg->setHopCount(msg->getHopCount()+1);
-
-
     int n = gateSize("gate");
-    int k = intuniform(0, n-1); //random forwarding
+    // Get the index of the arrival gate
+    int incomingGateIndex = msg->getArrivalGate()->getIndex();
+    EV << "Forwarding message " << msg << " to all gates except gate[" << incomingGateIndex << "]\n";
+    bool sent = false;
 
-    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
-    // $o and $i suffix is used to identify the input/output part of a two way gate
-    send(msg, "gate$o", k);
-}
+    // Forward the message to all gates except the incoming gate
+    for (int k = 0; k < n; ++k) {
+        if (k != incomingGateIndex) {
+            cMessage *copy = msg->dup();  // Create a duplicate of the message
+            send(copy, "gate$o", k);
+            sent = true;
+        }
+    }
+    if (!sent) {
+        delete msg;
+    }*/
+    int n = gateSize("gate");
 
-frame_BB *Bot_Builders_node5::generateMessage()
-{
-    char frameName[20];
-    int src = par("source");
-    int dest=par("destination");
-    sprintf(frameName, "tic-%d-to-%d", src, dest);
+        // Check if the message arrived through a gate or was scheduled
+        cGate *arrivalGate = msg->getArrivalGate();
+        int incomingGateIndex = (arrivalGate != nullptr) ? arrivalGate->getIndex() : -1;
 
-    // Create message object and set source and destination field.
-    frame_BB *msg = new frame_BB(frameName);
-    msg->setSource(src);
-    msg->setDestination(dest);
-    return msg;
+        EV << "Forwarding message " << msg->getName() << " to all gates except gate[" << incomingGateIndex << "]\n";
+
+        // Send the message to all gates except the incoming gate
+        for (int k = 0; k < n; ++k) {
+            if (k != incomingGateIndex) {
+                // Create a duplicate for each outgoing gate
+                cMessage *copy = msg->dup();
+                send(copy, "gate$o", k);  // Send the copy to gate[k]
+            }
+        }
+
+        // Delete the original message after forwarding
+        delete msg;
 }
